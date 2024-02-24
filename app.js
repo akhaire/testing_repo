d7be81f5eb62654e1a7ca1c34c2b105753449632
Ajay Khaire <akhaire1992@gmail.com>
2024-02-25 01:16:32 +0530

    Testting commit

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const {fs, createReadStream} = require('fs')
 http.createServer(function(req,res){
     // const text = fs.readFileSync('./content/bigfile.txt','utf8');
     // res.end(text)
-
-
     const filestream = createReadStream('./content/bigfile.txt','utf8')
     filestream.on('open',()=>{
         filestream.pipe(res)
